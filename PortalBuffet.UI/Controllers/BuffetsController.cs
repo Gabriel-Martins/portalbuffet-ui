e89593bc24524f5c4759b478c0ef4f7a2d3c660f
Gabriel Martins <gmartinsss@hotmail.com>
2022-11-06 09:20:27 -0300

    first commit

---
+++ PortalBuffet.UI/Controllers/BuffetsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortalBuffet.UI.Controllers
+{
+    public class BuffetsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BuffetPage()
+        {
+            return View();
+        }
+    }
+}
